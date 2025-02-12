5a298ab01711005a2ed360d4edbe394a3141e701
ndsimpson <63575139+ndsimpson@users.noreply.github.com>
2025-02-12 04:05:38 -0600

    Display Champion BCs too

--- components/live/bcs.py
+++ components/live/bcs.py
@@ -6,17 +6,16 @@ result = get_next_tournament()
 
 
 if result['days_until'] > 1:
-    st.write(f"<h2>Next Tournament is on {result['next_date']}</h2>", unsafe_allow_html=True)
-    st.write("Too soon to display upcoming battle conditions.  Try back tomorrow.")
+    st.markdown(f"# Next Tournament is on {result['next_date']}")
+    st.markdown("Too soon to display upcoming battle conditions.  Try back tomorrow.")
+    st.stop()
 elif result['days_until'] == 1:
-    st.write(f"<h2>Next Tournament is on {result['next_date']}</h2>", unsafe_allow_html=True)
-    st.write("The upcoming BCs are:", unsafe_allow_html=True)
-    battleconditions = predict_future_tournament(result['next_id'])
-    for battlecondition in battleconditions:
-        st.write(battlecondition)
+    st.markdown(f"# Next Tournament is on {result['next_date']}")
 else:
-    st.write("<h2>Tournament is today!</h2>", unsafe_allow_html=True)
-    st.write("The BCs are:", unsafe_allow_html=True)
-    battleconditions = predict_future_tournament(result['next_id'])
+    st.markdown("# Tournament is today!")
+
+for league in ["Legend", "Champion"]:
+    st.markdown(f"## The {league} BCs are:")
+    battleconditions = predict_future_tournament(result['next_id'], league)
     for battlecondition in battleconditions:
-        st.write(battlecondition)
+        st.markdown(f"* {battlecondition}")
