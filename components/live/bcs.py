56016950d858c2f7330ae799b76e61fb3c916c96
ndsimpson <63575139+ndsimpson@users.noreply.github.com>
2025-02-11 19:22:09 -0600

    I overrode too much

--- components/live/bcs.py
+++ components/live/bcs.py
@@ -6,15 +6,17 @@ result = get_next_tournament_id()
 
 
 if result['days_until'] > 1:
-    st.write(f"<h2>Next Tournament is in {result['days_until']} days on {result['next_tournament_date']}</h2>", unsafe_allow_html=True)
+    st.write(f"<h2>Next Tournament is on {result['next_tournament_date']}</h2>", unsafe_allow_html=True)
     st.write("Too soon to display upcoming battle conditions.  Try back tomorrow.")
+elif result['days_until'] == 1:
+    st.write(f"<h2>Next Tournament is on {result['next_tournament_date']}</h2>", unsafe_allow_html=True)
+    st.write("The upcoming BCs are:", unsafe_allow_html=True)
+    battleconditions = predict_future_tournament(result['next_tournament_id'])
+    for battlecondition in battleconditions:
+        st.write(battlecondition)
 else:
-    if result['days_until'] == 1:
-        st.write(f"<h2>Next Tournament is in {result['days_until']} day on {result['next_tournament_date']}</h2>", unsafe_allow_html=True)
-        st.write("The upcoming BCs are:", unsafe_allow_html=True)
-    else:
-        st.write(f"<h2>Tournament is today!</h2>", unsafe_allow_html=True)
-        st.write("The BCs are:", unsafe_allow_html=True)
+    st.write("<h2>Tournament is today!</h2>", unsafe_allow_html=True)
+    st.write("The BCs are:", unsafe_allow_html=True)
     battleconditions = predict_future_tournament(result['next_tournament_id'])
     for battlecondition in battleconditions:
         st.write(battlecondition)
