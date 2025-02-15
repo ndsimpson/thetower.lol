8cdf84c31fafa72a443b6f0192e0e31cb4302fbd
ndsimpson <63575139+ndsimpson@users.noreply.github.com>
2025-02-15 17:05:14 -0600

    Update BC page to show Platinum

--- components/live/bcs.py
+++ components/live/bcs.py
@@ -1,21 +1,23 @@
+"""Display tournament battle conditions in Streamlit interface."""
+
 import streamlit as st
-
-from dtower.tourney_results.bc import predict_future_tournament, get_next_tournament
-
-result = get_next_tournament()
+from dtower.tourney_results.bc import predict_future_tournament, get_tournament_info
 
 
-if result['days_until'] > 1:
-    st.markdown(f"# Next Tournament is on {result['next_date']}")
-    st.markdown("Too soon to display upcoming battle conditions.  Try back tomorrow.")
+tourney_id, tourney_date, days_until = get_tournament_info()
+
+if days_until > 1:
+    st.markdown(f"# Next Tournament is on {tourney_date}")
+    st.markdown("Too soon to display upcoming battle conditions. Try back tomorrow.")
     st.stop()
-elif result['days_until'] == 1:
-    st.markdown(f"# Next Tournament is on {result['next_date']}")
+elif days_until == 1:
+    st.markdown(f"# Next Tournament is on {tourney_date}")
 else:
     st.markdown("# Tournament is today!")
 
-for league in ["Legend", "Champion"]:
-    st.markdown(f"## The {league} BCs are:")
-    battleconditions = predict_future_tournament(result['next_id'], league)
-    for battlecondition in battleconditions:
-        st.markdown(f"* {battlecondition}")
+for league in ["Legend", "Champion", "Platinum"]:
+    st.header(f"The {league} BCs are:", divider=True)
+    # st.markdown(f"## The {league} BCs are:")
+    battle_conditions = predict_future_tournament(tourney_id, league)
+    for condition in battle_conditions:
+        st.markdown(f"* {condition}")
