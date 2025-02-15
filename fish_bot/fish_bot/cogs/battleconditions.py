8b6f699fc5e1d7b09b9b2d010a0c1e3adf6e1b75
ndsimpson <63575139+ndsimpson@users.noreply.github.com>
2025-02-15 17:28:49 -0600

    Update BC cog to use new tourney info function format

--- fish_bot/fish_bot/cogs/battleconditions.py
+++ fish_bot/fish_bot/cogs/battleconditions.py
@@ -5,11 +5,12 @@ from discord.ext import commands, tasks
 from fish_bot import const
 from fish_bot.util import is_allowed_user
 
-from dtower.tourney_results.bc import predict_future_tournament, get_next_tournament
+from dtower.tourney_results.bc import predict_future_tournament, get_tournament_info
 
 league_threads = {
     "Legend" : const.legend_bc_thread_id,
-    "Champion" : const.champ_bc_thread_id
+    "Champion" : const.champ_bc_thread_id,
+    "Platinum" : const.plat_bc_thread_id
 }
 
 
@@ -29,21 +30,21 @@ class BattleConditions(commands.Cog, name="BattleConditions"):
 
     @commands.command()
     async def get_tourneyday(self, ctx):
-        result = get_next_tournament()
-        if result['days_until'] == 0:
+        tourney_id, tourney_date, days_until = get_tournament_info()
+        if days_until == 0:
             await ctx.send("The tournament is today!")
         else:
-            await ctx.send(f"The tournament is {result['days_until']} days away and will run on {result['next_date']}")
+            await ctx.send(f"The tournament is {days_until} days away and will run on {tourney_date}")
 
     @commands.command()
     @is_allowed_user(const.id_pog, const.id_fishy)
     async def get_battleconditions(self, ctx, league: str = "Legend"):
-        result = get_next_tournament()
-        battleconditions = predict_future_tournament(result['next_id'], league)
-        if result['days_until'] == 0:
+        tourney_id, tourney_date, days_until = get_tournament_info()
+        battleconditions = predict_future_tournament(tourney_id, league)
+        if days_until == 0:
             message = f"The BCs for today's {league} tourney are:\n"
         else:
-            message = f"The BCs for the {league} tourney on {result['next_date']} are:\n"
+            message = f"The BCs for the {league} tourney on {tourney_date} are:\n"
         for battlecondition in battleconditions:
             message += f"- {battlecondition}\n"
         await ctx.message.delete()
@@ -51,11 +52,11 @@ class BattleConditions(commands.Cog, name="BattleConditions"):
 
     @tasks.loop(time=datetime.time(hour=0, minute=0))
     async def scheduled_bc_messages(self):
-        result = get_next_tournament()
-        if result['days_until'] == 1:
-            for league in ["Legend", "Champion"]:
-                battleconditions = predict_future_tournament(result['next_id'])
-                message = f"The BCs for tomorrow's {league} tournament ({result['next_date']}) are:\n"
+        tourney_id, tourney_date, days_until = get_tournament_info()
+        if days_until == 1:
+            for league in ["Legend", "Champion", "Platinum"]:
+                battleconditions = predict_future_tournament(tourney_id)
+                message = f"The BCs for tomorrow's {league} tournament ({tourney_date}) are:\n"
                 for battlecondition in battleconditions:
                     message += f"- {battlecondition}\n"
                 channel = self.bot.get_channel(league_threads[league])
