e583da2c6db750a09941c3e6737fcfb24704e531
ndsimpson <63575139+ndsimpson@users.noreply.github.com>
2025-02-19 15:46:28 -0600

    Update display of BCs

--- components/live/bcs.py
+++ components/live/bcs.py
@@ -1,9 +1,10 @@
 """Display tournament battle conditions in Streamlit interface."""
 
-import streamlit as st
 from datetime import timedelta
-from towerbcs.towerbcs import predict_future_tournament, TournamentPredictor
+import pandas as pd
+import streamlit as st
 from dtower.tourney_results.constants import leagues
+from towerbcs.towerbcs import predict_future_tournament, TournamentPredictor
 
 tourney_id, tourney_date, days_until = TournamentPredictor.get_tournament_info()
 
@@ -11,14 +12,13 @@ if days_until > 1:
     st.markdown(f"# Next Tournament is on {tourney_date}")
     st.markdown(f"Too soon to display upcoming battle conditions. Try back on {tourney_date - timedelta(days=1)}.")
     st.stop()
-elif days_until == 1:
-    st.markdown(f"# Next Tournament is on {tourney_date}")
-else:
-    st.markdown("# Tournament is today!")
 
-for league in leagues:
-    st.header(f"The {league} BCs are:", divider=True)
-    # st.markdown(f"## The {league} BCs are:")
-    battle_conditions = predict_future_tournament(tourney_id, league)
-    for condition in battle_conditions:
-        st.markdown(f"* {condition}")
+st.markdown(f"# Tournament {'is today!' if days_until == 0 else f'is on {tourney_date}'}")
+
+st.dataframe(
+    pd.DataFrame.from_dict(
+        {league: predict_future_tournament(tourney_id, league) for league in leagues},
+        orient='index'
+    ).transpose().fillna(''),
+    use_container_width=True
+)
