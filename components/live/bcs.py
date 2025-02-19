eaca3425797bbfe47599eac0a945792b8810ae08
ndsimpson <63575139+ndsimpson@users.noreply.github.com>
2025-02-19 16:07:04 -0600

    Update bcs.py

--- components/live/bcs.py
+++ components/live/bcs.py
@@ -8,12 +8,13 @@ from towerbcs.towerbcs import predict_future_tournament, TournamentPredictor
 
 tourney_id, tourney_date, days_until = TournamentPredictor.get_tournament_info()
 
+st.markdown("# Battle Conditions")
 if days_until > 1:
-    st.markdown(f"# Next Tournament is on {tourney_date}")
+    st.markdown(f"## Next Tournament is on {tourney_date}")
     st.markdown(f"Too soon to display upcoming battle conditions. Try back on {tourney_date - timedelta(days=1)}.")
     st.stop()
 
-st.markdown(f"# Tournament {'is today!' if days_until == 0 else f'is on {tourney_date}'}")
+st.markdown(f"## Tournament {'is today!' if days_until == 0 else f'is on {tourney_date}'}")
 
 st.dataframe(
     pd.DataFrame.from_dict(
