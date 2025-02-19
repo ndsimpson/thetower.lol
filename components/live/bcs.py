35b484e54c6c66956073ed72cdedbdf3159b42c4
ndsimpson <63575139+ndsimpson@users.noreply.github.com>
2025-02-19 15:08:28 -0600

    Account for copper

--- components/live/bcs.py
+++ components/live/bcs.py
@@ -3,7 +3,7 @@
 import streamlit as st
 from datetime import timedelta
 from towerbcs.towerbcs import predict_future_tournament, TournamentPredictor
-
+from dtower.tourney_results.constants import leagues
 
 tourney_id, tourney_date, days_until = TournamentPredictor.get_tournament_info()
 
@@ -16,7 +16,7 @@ elif days_until == 1:
 else:
     st.markdown("# Tournament is today!")
 
-for league in ["Legend", "Champion", "Platinum"]:
+for league in leagues:
     st.header(f"The {league} BCs are:", divider=True)
     # st.markdown(f"## The {league} BCs are:")
     battle_conditions = predict_future_tournament(tourney_id, league)
