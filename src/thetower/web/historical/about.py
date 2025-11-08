a0b4ce1b832883f855f1a31be219d0a91b648179
ndsimpson <63575139+ndsimpson@users.noreply.github.com>
2025-11-08 16:20:08 -0600

    Update and simplify the about page.

--- src/thetower/web/historical/about.py
+++ src/thetower/web/historical/about.py
@@ -3,19 +3,11 @@ import streamlit as st
 
 def compute_about(*args, **kwargs):
     st.title("About")
-    st.markdown("Currenly ran by the discord team.  Primary contact: `thedisasterfish`")
-    st.markdown("Special thanks to `_xskye_` for working out the info for battle conditions.")
+    st.markdown("This project is primarily managed by `thedisasterfish`, in collaboration with the Discord and Reddit moderation teams.")
     st.markdown("Site orignally created by `098799` ('this guy' on discord) 2022-2024.")
-    st.markdown("Thanks to `andreasjn` for the help with the discord bot.")
-    st.markdown("Thanks to `Milamber33` for a lot of help with css and other things.")
-    st.markdown("Thanks to `Jim808`, `ObsUK` and `Bartek` for a graph ideas and encouragement.")
-    st.markdown("Thanks to `Fnord`, `Neothin87`, and others for the encouragement and ideas")
-    st.markdown("Thanks to `Pog` and the discord mods for all the work on sus reports.")
-
-    st.write("If you have any questions of concerns regarding privacy, including opt outs, please contact fish@thetower.lol.")
-
+    st.markdown("Special thanks to these people, in no particular order: `_xskye_`, `andreasjn`, `Jim808`, `ObsUK`, `Bartek`, `Milamber33`, `Fnord`, and `Neothin87`.")
     st.write(
-        "DISCLAIMER:  This is a project by TheDisasterFish and friends.  All work is best effort and strictly voluntary.  Inclusion in the results isn't guaranteed and we make no warranties as to the site accuracy."
+        "DISCLAIMER:  All work is best effort and strictly voluntary.  Inclusion in the results isn't guaranteed and we make no warranties as to the site accuracy."
     )
 
 
